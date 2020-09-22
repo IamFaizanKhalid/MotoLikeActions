47ea7b47e1c001bc3acd6aab2ca50d8cc28f834e
Faizan Khalid <IamFaizanKhalid@gmail.com>
2020-09-22 10:41:50 +0500

    Added Separate Shake Detector Class

--- app/src/main/java/com/faizankhalid/actions/MainActivity.java
+++ app/src/main/java/com/faizankhalid/actions/MainActivity.java
@@ -3,13 +3,13 @@ package com.faizankhalid.actions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements ShakeDetector.OnShakeListener {
+	
+	ShakeDetector shake;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,35 +22,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 		Sensor accel;
 		try {
 			accel = sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-			sMgr.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+			shake = new ShakeDetector();
+			sMgr.registerListener(shake, accel, SensorManager.SENSOR_DELAY_NORMAL);
+			shake.setOnShakeListener(this);
 		}catch (NullPointerException np){
 			Toast.makeText(this, "Accelerometer Sensor Not Found", Toast.LENGTH_SHORT).show();
 			np.printStackTrace();
 		}
 	}
 	
-	private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
-	
 	@Override
-	public void onSensorChanged(SensorEvent event) {
-			float x = event.values[0];
-			float y = event.values[1];
-			float z = event.values[2];
-			
-			float gX = x / SensorManager.GRAVITY_EARTH;
-			float gY = y / SensorManager.GRAVITY_EARTH;
-			float gZ = z / SensorManager.GRAVITY_EARTH;
-			
-			// gForce will be close to 1 when there is no movement.
-			float gForce = (float)Math.sqrt(gX * gX + gY * gY + gZ * gZ);
-			
-			if (gForce > SHAKE_THRESHOLD_GRAVITY) {
-				Toast.makeText(this, "Sensor Sensed Nonsense", Toast.LENGTH_SHORT).show();
-			}
-		}
-	
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	public void onShake(int count) {
+		Toast.makeText(this,"Shook "+count+" times",Toast.LENGTH_SHORT).show();
 	}
-	
 }
